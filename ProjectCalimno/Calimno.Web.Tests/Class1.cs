b966f8392a51b8491801672c7c3d63717e24819f
Gianluca Cerritelli <gianluca.cerritelli@gmail.com>
2017-04-21 15:01:38 +0100

    Adding Unit tests

---
+++ ProjectCalimno/Calimno.Web.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Calimno.Web.Tests
+{
+	[TestFixture]
+    public class SampleTests
+    {
+	    [Test]
+	    public void OneEqualsOne()
+	    {
+		    Assert.AreEqual(1,1);
+	    }
+
+	    [Test]
+	    public void ZeroNotEqualOne()
+	    {
+		    Assert.AreNotEqual(0,1);
+	    }
+    }
+}
